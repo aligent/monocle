94e6fdcdd6666776cdc4a7bf5cdbb974d8306b84
Daniel van der Ploeg <daniel.vanderploeg@aligent.com.au>
2026-05-07 11:40:56 +0930

    Wire new metric into metricGet dispatch table
    Server.hs:923 has a hand-coded case mapping metric names to
    implementations for the metric/get endpoint. allMetrics in Queries.hs
    only feeds metric/list and metric/info, so adding a metric there
    isn't enough to make it executable.

--- src/Monocle/Api/Server.hs
+++ src/Monocle/Api/Server.hs
@@ -939,6 +939,7 @@ metricGet auth request = checkAuth auth response
       "time_to_merge" -> runMetric Q.metricTimeToMerge
       "time_to_merge_variance" -> runMetric Q.metricTimeToMergeVariance
       "first_review_mean_time" -> runMetric Q.metricFirstReviewMeanTime
+      "first_review_to_last_approval_mean_time" -> runMetric Q.metricFirstReviewToLastApprovalMeanTime
       "first_comment_mean_time" -> runMetric Q.metricFirstCommentMeanTime
       "first_reviewer_mean_time" -> runMetric Q.metricFirstReviewerMeanTime
       "first_commenter_mean_time" -> runMetric Q.metricFirstCommenterMeanTime
